9e70f93ced01b81c9758a975a05fa0ca49f558c8
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-20 01:08:18 -0400

    modified program.cs

--- comp123-S2017-lesson9b/Program.cs
+++ comp123-S2017-lesson9b/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * name: mehul khosla
+ * date:july 20, 2017
+ * description: demo lesson 9
+ * version:0.1 - created the program class
+ * /
 namespace comp123_S2017_lesson9b
 {
     class Program
