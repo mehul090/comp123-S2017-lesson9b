1da75894c8470f2938477196244d6aeb65348249
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-20 01:37:29 -0400

    added super human class

---
+++ comp123-S2017-lesson9b/SuperHuman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * name: mehul khosla
+ * date:july 20, 2017
+ * description: this is the super human class that extends the human abstracts class
+ * version:0.1 - created the super human class
+ */
+namespace comp123_S2017_lesson9b
+{
+    /// <summary>
+    /// this is the super human class
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // private instance variable
+
+        // public properties
+
+        //constructors
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+
+        //private methods
+
+        //public methods
+    }
+}
