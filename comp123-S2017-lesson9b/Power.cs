74723d75105686286759b53b6c22d7d92d873608
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-20 01:42:02 -0400

    added power class

---
+++ comp123-S2017-lesson9b/Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * name: mehul khosla
+ * date:july 20, 2017
+ * description: demo for lesson 9
+ * version:0.1 - created the powerclass
+ */
+namespace comp123_S2017_lesson9b
+{
+    /// <summary>
+    /// this is the power class class which will be used as a datatype
+    /// </summary>
+    public class Power
+    {
+        //public properties
+        public string Nmae { get; set; }
+        public int Rank { get; set; }
+    }
+}
