d04a6920530f346d5cac4932ecd0ee37af6fe73e
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-20 23:55:18 -0400

    added IHasKarma interface

---
+++ comp123-S2017-lesson9b/IHasKarma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * name: mehul khosla
+ * date:july 20, 2017
+ * description: this is an interface that defines a karma property that
+ * must be implmentes in any class that subscribr to it
+ * version:0.1- created interface iHasKarma
+ */
+namespace comp123_S2017_lesson9b
+{
+    /// <summary>
+    /// this is the IHasKarma
+    /// </summary>
+    public interface IHasKarma
+    {
+                //public properties
+        int Karma { get; set; }
+    }
+}
