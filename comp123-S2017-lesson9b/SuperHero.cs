3cf79ee7f686790224815611e0e4db8b72d9a440
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-21 00:04:14 -0400

    added SuperHero class

---
+++ comp123-S2017-lesson9b/SuperHero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * name: mehul khosla
+ * date:july 20, 2017
+ * description: this is the super hero class which inherits from the SuperHuman class
+ * and implements the IHasKarma interface
+ * version 0.1 - created the SuperHero class
+ */
+namespace comp123_S2017_lesson9b
+{
+    /// <summary>
+    /// this is the SuperHuman class
+    /// </summary>
+    public class SuperHero : SuperHuman,IHasKarma
+    {
+        //private instsance variable
+        private int _Karma;
+
+        //public properties
+        public int Karma
+        {
+            get { return this._Karma; }
+            set { this._Karma = value; }
+        }
+
+        /// <summary>
+        /// this is the main cnstuctor for super human class
+        /// it takes one argument - name
+        /// </summary>
+        /// <param name="name"></param>
+        //constructors
+        public SuperHero (string name)
+            :base(name)
+        {
+
+        }
+
+
+        //private methods
+
+        //public methods
+    }
+}
